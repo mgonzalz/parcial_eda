2122c06b545e730d7af132c8956f18b3ae0e03fa
BobiStudio <mgonzgarc@myuax.com>
2023-03-24 12:02:20 +0100

    ej02

--- ejercicio02/ejercicio02.py
+++ ejercicio02/ejercicio02.py
@@ -32,10 +32,36 @@ class Mamifero(Animal):
 class Gato(Mamifero):
     def __init__(self, nombre, edad):
         super().__init__(nombre, edad, 'mamífero')
+        self.animal = 'gato'
     def __str__(self):
-        return self.nombre + ' es un ' + self.tipo + ' de ' + str(self.edad) + ' años.'
+        return self.nombre + ' es un ' + self.animal + ' ' + self.tipo + ' de ' + str(self.edad) + ' años.'
 
+class Oviparo(Animal):
+    def __init__(self, nombre, edad, tipo):
+        super().__init__(nombre, edad)
+        self.tipo = tipo
+    def tipo(self):
+        return self.tipo
+
+class Pollo(Oviparo):
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad, 'ovíparo')
+        self.animal = 'pollo'
+    def __str__(self):
+        return self.nombre + ' es un ' + self.animal + ' ' + self.tipo + ' de ' + str(self.edad) + ' años.'
+
+class Ornitorrinco(Mamifero):
+    def __init__(self, nombre, edad):
+        super().__init__(nombre, edad, 'mamífero')
+        self.animal = 'ornitorrinco'
+    def __str__(self):
+        return self.nombre + ' es un ' + self.animal + ' ' + self.tipo + 'y ovíparo de ' + str(self.edad) + ' años.'
 #PROGRAMA PRINCIPAL:
 gato1 = Gato('Sebastian', 5)
 print(gato1)
 
+pollo1 = Pollo('Piolín', 2)
+print(pollo1)
+
+ornitorrinco1 = Ornitorrinco('Perry', 1)
+print(ornitorrinco1)
